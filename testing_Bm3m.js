f1ff855bfaaa70b6051647ff8a27c857c6427b66
Doniyor Rustamov <132898784+Doniyor6444@users.noreply.github.com>
2026-01-02 20:44:41 +0500

    Refactor script loading method in testing_Bm3m.js

--- testing_Bm3m.js
+++ testing_Bm3m.js
@@ -1,10 +1,5 @@
-(function() {
-    // 1. Savolni topish (Hozircha saytdagi barcha matnni oladi)
-    let savol = document.body.innerText.substring(0, 200); 
-
-    // 2. Ma'lumotni kodlash
-    let data = btoa(encodeURIComponent(savol + '[:]' + 'variantlar'));
-
-    // 3. Local faylni ochish
-    window.open(`http://127.0.0.1:5501/local.html?data=${data}`, 'helper', 'width=200,height=200');
+javascript:(function(){
+    var s = document.createElement('script');
+    s.src = 'https://doniyor.github.io/my-test-helper/testing_Bm3m.js?v=' + Math.random();
+    document.body.appendChild(s);
 })();
