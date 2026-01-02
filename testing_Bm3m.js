640a12aeb5a4fe8704fd4aac9f94f2394f29244c
Doniyor Rustamov <132898784+Doniyor6444@users.noreply.github.com>
2026-01-02 20:49:37 +0500

    Update testing_Bm3m.js

--- testing_Bm3m.js
+++ testing_Bm3m.js
@@ -1,5 +1,11 @@
-javascript:(function(){
-    var s = document.createElement('script');
-    s.src = 'https://doniyor.github.io/my-test-helper/testing_Bm3m.js?v=' + Math.random();
-    document.body.appendChild(s);
+(function() {
+    // 1. Savolni yig'ish
+    let savol = document.body.innerText.substring(0, 300); 
+    let data = btoa(encodeURIComponent(savol + '[:]' + 'variantlar'));
+
+    // 2. SIZNING LINKINGIZ (doniyor6444 ekanini hisobga olgan holda)
+    // REPO_NOMI joyiga GitHub'dagi proyekt nomini yozing
+    let onlineLink = "https://doniyor6444.github.io/REPO_NOMI/local.html";
+    
+    window.open(`${onlineLink}?data=${data}`, 'helper', 'width=200,height=200,left=0,top=0');
 })();
